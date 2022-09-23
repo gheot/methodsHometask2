5c84affb532050050df68d2823d51d35fefb2565
nilova <marina.nilova@yandex.ru>
2022-09-23 17:10:28 +0300

    Howework is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
-
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int year = 2022;
+        leapYear(year);
+        String clientOS = "Android";
+        int YourDeviceYear = 2021;
+        checkOS(clientOS, YourDeviceYear);
+        deliveryDaysCounter();
+
+
     }
+
+    public static void leapYear(int a) {
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+            System.out.println(a + " - високосный год");
+
+        } else {
+            System.out.println(a + " - не високосный год");
+        }
+
+    }
+
+    public static void checkOS(String OS, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (OS.equals("IOS")) {
+            System.out.println("IOS");
+        } else {
+            System.out.println("Android");
+        }
+        if (currentYear < deviceYear) {
+            System.out.println("Установите lite-версию для " + OS);
+        } else {
+            System.out.println("Установите обычную версию для " + OS);
+        }
+    }
+
+    public static void deliveryDaysCounter() {
+        int daysCounter = 1;
+        int deliveryDistance = 5;
+        if (deliveryDistance > 20) {
+            daysCounter++;
+        }
+        if (deliveryDistance > 60) {
+            daysCounter++;
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("На это расстояние мы не доставляем");
+        }
+        System.out.println("Потребуется дней: " + daysCounter);
+    }
+
 }
